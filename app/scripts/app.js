63415fb601dce5e65ca93b3019ff36177bb3da39
Mike Zevitas <mzevitas@gmail.com>
2015-06-10 22:42:47 -0400

    angular app for likes and post

---
+++ app/scripts/app.js
@@ -0,0 +1,64 @@
+(function() {
+
+    'use strict';
+
+    var app = angular.module('News', []);
+
+
+
+
+    app.factory('posts', [function() {
+        var o = {
+            posts: []
+        };
+        return o;
+    }]);
+
+
+    
+
+    app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
+
+        $scope.posts = [{
+            title: 'post 1',
+            upvotes: 5
+        }, {
+            title: 'post 2',
+            upvotes: 2
+        }, {
+            title: 'post 3',
+            upvotes: 15
+        }, {
+            title: 'post 4',
+            upvotes: 9
+        }, {
+            title: 'post 5',
+            upvotes: 4
+        }];
+
+
+        $scope.addPost = function() {
+            if (!$scope.title || $scope.title === '') {
+                return;
+            }
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0
+            });
+            $scope.title = '';
+            $scope.link = '';
+        };
+
+
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        };
+        $scope.incrementDownvotes = function(post) {
+            post.upvotes -= 1;
+        };
+    }]);
+
+
+
+}());
